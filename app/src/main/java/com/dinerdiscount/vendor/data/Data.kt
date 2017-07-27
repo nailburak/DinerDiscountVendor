e7b28ead8022a1083c1fe3fa4a59bc3f9828e8ce
Burak <nburaktaban@gmail.com>
2017-07-26 20:59:17 -0400

    first commit

---
+++ app/src/main/java/com/dinerdiscount/vendor/data/Data.kt
@@ -0,0 +1,8 @@
+package com.dinerdiscount.vendor.data
+
+/**
+ * Created by nbura on 2017-07-26.
+ */
+
+data class SeekBars(val key: String, val discount: Int)
+val seekBarArray = ArrayList<SeekBars>()
---
+++ app/src/main/java/com/dinerdiscount/vendor/fragments/TodaysDiscountFragment.kt
@@ -0,0 +1,20 @@
+package com.dinerdiscount.vendor.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.dinerdiscount.vendor.R
+
+class TodaysDiscountFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_todays_discount, container, false)
+    }
+
+
+}
